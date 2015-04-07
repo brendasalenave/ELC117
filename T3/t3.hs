896d496465264b9a2be452b5e94bde9c39084077
brendasalenave <brendasalenave@live.com>
2015-04-06 23:44:58 -0300

    T3
    up

--- T3/t3.hs
+++ T3/t3.hs
@@ -1,9 +1,9 @@
 import Data.Char
 
-{- 1. Escreva uma funÁ„o recursiva firstName :: String -> String que,
+{- 1. Escreva uma fun√ß√£o recursiva firstName :: String -> String que,
  dado o nome completo de uma pessoa, obtenha seu primeiro nome.
- Suponha que cada parte do nome seja separada por um espaÁo e que
- n„o existam espaÁos no inÌcio ou fim do nome.-}
+ Suponha que cada parte do nome seja separada por um espa√ßo e que
+ n√£o existam espa√ßos no in√≠cio ou fim do nome.-}
 
 firstName :: String -> String
 firstName name
@@ -11,34 +11,34 @@ firstName name
 	|otherwise = (head name) : firstName(tail name)
 
 
-{- 2. Escreva uma funÁ„o firstName' :: String -> String com o mesmo
- resultado do exercÌcio anterior, mas sem usar recurs„o.-}
+{- 2. Escreva uma fun√ß√£o firstName' :: String -> String com o mesmo
+ resultado do exerc√≠cio anterior, mas sem usar recurs√£o.-}
 
 firstName2 :: String -> String
 firstName2 name1 = takeWhile (/= ' ') name1
 
 
-{- 3.Escreva uma funÁ„o lastName :: String -> String que, dado o nome
- completo de uma pessoa, obtenha seu ˙ltimo sobrenome. Suponha que
- cada parte do nome seja separada por um espaÁo e que n„o existam
- espaÁos no inÌcio ou fim do nome.-}
+{- 3.Escreva uma fun√ß√£o lastName :: String -> String que, dado o nome
+ completo de uma pessoa, obtenha seu √∫ltimo sobrenome. Suponha que
+ cada parte do nome seja separada por um espa√ßo e que n√£o existam
+ espa√ßos no in√≠cio ou fim do nome.-}
 
 lastName :: String -> String
 lastName lname = (reverse (takeWhile (/= ' ') (reverse lname)))
 
 
-{- 4.Escreva uma funÁ„o n„o-recursiva userName :: String -> String que,
- dado o nome completo de uma pessoa, crie um nome de usu·rio (login)
+{- 4.Escreva uma fun√ß√£o n√£o-recursiva userName :: String -> String que,
+ dado o nome completo de uma pessoa, crie um nome de usu√°rio (login)
  da pessoa, formado por: primeira letra do nome seguida do sobrenome,
- tudo em min˙sculas. Dica: estude as funÁıes prÈ-definidas no mÛdulo
- Data.Char, para manipulaÁ„o de mai˙sculas e min˙sculas.-}
+ tudo em min√∫sculas. Dica: estude as fun√ß√µes pr√©-definidas no m√≥dulo
+ Data.Char, para manipula√ß√£o de mai√∫sculas e min√∫sculas.-}
 
 userName :: String -> String
 userName uname = map toLower ((head uname) : (reverse (takeWhile (/= ' ') (reverse uname))))
 
 
 --ARRUMAR
-{- 5.Escreva uma funÁ„o n„o-recursiva encodeName :: String -> String que
+{- 5.Escreva uma fun√ß√£o n√£o-recursiva encodeName :: String -> String que
  substitua vogais em uma string, conforme o esquema a seguir: a = 4,
  e = 3, i = 1, o = 0, u = 00. -}
 
@@ -50,7 +50,7 @@ encodeName eName =  [(if x == 'a' || x =='A' then '4' else
 					if x == 'u' || x == 'U' then '?' else x) | x <- eName]
 
 
-{- 6.Escreva uma funÁ„o isElem :: Int -> [Int] -> Bool que verifique se
+{- 6.Escreva uma fun√ß√£o isElem :: Int -> [Int] -> Bool que verifique se
  um dado elemento pertence a uma lista, conforme os exemplos: -}
 
 isElem :: Int -> [Int] -> Bool
@@ -60,30 +60,36 @@ isElem x lista
 	|otherwise = isElem x (tail lista)
 
 
-{-7.Escreva uma funÁ„o recursiva que retorne o n˙mero de vogais em uma string -}
+{-7.Escreva uma fun√ß√£o recursiva que retorne o n√∫mero de vogais em uma string -}
 
 countVowel :: String -> Int
 countVowel [] =0
-countVowel str = (count(head str)) + (countVowel(tail str))
-	where count str
-		|str == 'a' = 1
-		|str == 'e' = 1
-		|str == 'i' = 1
-		|str == 'o' = 1
-		|str == 'u' = 1
-		|otherwise = 0 
+countVowel str  
+		|((head str) == 'a') || ((head str) == 'A') = 1 + countVowel(tail str)
+		|((head str) == 'e') || ((head str) == 'E') = 1 + countVowel(tail str)
+		|((head str) == 'i') || ((head str) == 'I') = 1 + countVowel(tail str)
+		|((head str) == 'o') || ((head str) == 'O') = 1 + countVowel(tail str)
+		|((head str) == 'u') || ((head str) == 'U') = 1 + countVowel(tail str)
+		|otherwise = 0 + countVowel(tail str) 
 
 
-{-8.Escreva uma funÁ„o n„o-recursiva que retorne o n˙mero de consoantes
+{-8.Escreva uma fun√ß√£o n√£o-recursiva que retorne o n√∫mero de consoantes
  em uma string.                                                        -}
 
 countCons :: String -> Int
 countCons str = length ([x | x <- str, x /= 'a', x /= 'e', x /= 'i', x /= 'o', x /= 'u'])
 
-{- 9.Escreva uma funÁ„o n„o-recursiva isInt :: String -> Bool que verifique
- se uma dada string sÛ contÈm dÌgitos (0 a 9).							-}
+{- 9.Escreva uma fun√ß√£o n√£o-recursiva isInt :: String -> Bool que verifique
+ se uma dada string s√≥ cont√©m d√≠gitos (0 a 9).							-}
 
 isInt :: String -> Bool
-isInt str 
-	|((length str) == (length([x | x<-str, x == '0', x == '1', x == '2', x == '3', x == '4', x == '5', x == '6', x == 7, x == '8', x == '9'))) = True
+isInt [] = False
+isInt str
+	|length(str)== length(filter (\str->(str=='0')||(str=='1')||(str=='2')||(str=='3')||(str=='4')||(str=='5')||(str=='6')||(str=='7')||(str=='8')||(str=='9')) str) = True
 	|otherwise = False
+
+
+{- 10.Escreva uma fun√ß√£o n√£o-recursiva strToInt :: String -> Int que converta
+ uma string em um n√∫mero inteiro, fazendo opera√ß√µes aritm√©ticas com seus d√≠gitos
+ (p.ex.: "356" = 3*100 + 5*10 + 6*1 = 356). Considere que a string seja um n√∫mero
+ v√°lido, isto √©, s√≥ contenha d√≠gitos de 0 a 9. Dica: se n√£o souber por onde come√ßar, estude o exemplo de valida√ß√£o de CPF visto em aula.-}
