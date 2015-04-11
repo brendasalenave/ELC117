4e58f79d9ce50e4cf3bd09b7a769c8757cdc7150
brendasalenave <brendasalenave@live.com>
2015-04-11 12:54:37 -0300

    Lista de logins
    Ok

--- T3/listaLogins.hs
+++ T3/listaLogins.hs
@@ -5,10 +5,9 @@ main = do
     strcontent <- readFile "nomes.csv"
     let strlist = lines strcontent
     	user = userName (strlist)
-        strnew = "@inf.ufsm.br":"str2":user
+        strnew = [x++"@inf.ufsm.br" | x<-user]
     writeFile "logins.csv" (unlines strnew)
 
--- aplicar essa função para uma lista de strings
 userName :: [String] -> [String]
 userName [] = []
 userName uname = [map toLower ((head x) : (reverse (takeWhile (/= ' ') (reverse x)))) | x<- uname] 
