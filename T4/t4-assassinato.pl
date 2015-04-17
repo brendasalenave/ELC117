b56e08e3d3064835c8d986a5d7d6505244e7eda9
brendasalenave <brendasalenave@live.com>
2015-04-17 20:16:01 -0300

    T4

---
+++ T4/t4-assassinato.pl
@@ -0,0 +1,70 @@
+vitima(anita).
+
+insano(maria).
+insano(adriano).
+
+pobre(bernardo).
+pobre(bia).
+pobre(pedro).
+pobre(maria).
+
+rico(alice).
+rico(henrique).
+rico(adriano).
+
+relacionamento(anita, pedro).
+relacionamento(anita, bernardo).
+relacionamento(caren, bernardo).
+relacionamento(caren, adriano).
+relacionamento(maria, adriano).
+relacionamento(maria, henrique).
+relacionamento(alice, henrique).
+relacionamento(alice, pedro).
+
+segunda_feira(pedro, santa_maria).
+terca_feira(pedro, santa_maria).
+quarta_feira(pedro, porto_alegre).
+quinta_feira(pedro, santa_maria).
+sexta_feira(pedro, apartamento).
+
+segunda_feira(caren, porto_alegre).
+terca_feira(caren, porto_alegre).
+quarta_feira(caren, porto_alegre).
+quinta_feira(caren, santa_maria).
+sexta_feira(caren, apartamento).
+
+segunda_feira(henrique, apartamento).
+terca_feira(henrique, porto_alegre).
+quarta_feira(henrique, apartamento).
+quinta_feira(henrique, apartamento).
+sexta_feira(henrique, apartamento).
+
+segunda_feira(bia, apartamento).
+terca_feira(bia, porto_alegre).
+quarta_feira(bia, porto_alegre).
+quinta_feira(bia, santa_maria).
+sexta_feira(bia, apartamento).
+
+segunda_feira(adriano, apartamento).
+terca_feira(adriano, apartamento).
+quarta_feira(adriano, santa_maria).
+quinta_feira(adriano, apartamento).
+sexta_feira(adriano, apartamento).
+
+segunda_feira(alice, apartamento).
+terca_feira(alice, porto_alegre).
+quarta_feira(alice, porto_alegre).
+quinta_feira(alice, apartamento).
+sexta_feira(alice, apartamento).
+
+segunda_feira(bernardo, santa_maria).
+terca_feira(bernardo, santa_maria).
+quarta_feira(bernardo, porto_alegre).
+quinta_feira(bernardo, santa_maria).
+sexta_feira(bernardo, apartamento).
+
+segunda_feira(maria, apartamento).
+terca_feira(maria, santa_maria).
+quarta_feira(maria, santa_maria).
+quinta_feira(maria, santa_maria).
+sexta_feira(maria, apartamento).
