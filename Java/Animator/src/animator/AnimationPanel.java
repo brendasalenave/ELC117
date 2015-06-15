fb56d9a323765d764f9065d3bfc9ad34816bdde5
brendasalenave <brendasalenave@live.com>
2015-06-14 23:50:01 -0300

    Update

--- Java/Animator/src/animator/AnimationPanel.java
+++ Java/Animator/src/animator/AnimationPanel.java
@@ -1,50 +1,48 @@
 package animator;
 
+import animator.ObjectSet;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
-import javax.swing.BorderFactory;
 import javax.swing.JPanel;
 
-// Area onde eh mostrada a animacao.
-class AnimationPanel extends JPanel implements Runnable {
-
-    private Dimension dim;
+class AnimationPanel extends JPanel
+implements Runnable {
+    private Dimension tam;
     private ObjectSet objs;
     private int millis;
 
-    public AnimationPanel(ObjectSet objs, Dimension dim, int millis) {
-        this.setBackground(Color.black);
+    public AnimationPanel(ObjectSet objs, Dimension tam, int millis) {
+        this.setBackground(Color.gray);
         this.objs = objs;
-        this.dim = dim;
+        this.tam = tam;
         this.millis = millis;
     }
 
     @Override
     public Dimension getPreferredSize() {
-        return dim;
+        return this.tam;
     }
 
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-        objs.drawAll(g);
+        this.objs.drawAll(g);
     }
 
     @Override
     public void run() {
-        boolean isRunning = true;
-        System.out.println("Running");
-        while (isRunning) {
-            objs.moveAll();
-            repaint();
+        boolean executa = true;
+        
+        while (executa) {
+            this.objs.moveAll();
+            this.repaint();
             try {
-                Thread.sleep(millis);
-            } catch (InterruptedException e) {
-                isRunning = false;
-                System.out.println("Stopped");
+                Thread.sleep(this.millis);
+            }
+            catch (InterruptedException e) {
+                executa = false;
             }
         }
     }
 }
-
